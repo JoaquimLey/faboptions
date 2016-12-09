4a7ad10e017073cd085d921c10f5d1287b4ab97b
Joaquim Ley <joaquimley@gmail.com>
2016-12-09 18:14:10 +0000

    Version 1.0.2 (#13)
    * Update library version on sample, add description to deploy script, update README
    
    * Sample version code bump
    
    * Standard code style, fix offset in close icon state
    
    * Library version bump
    
    * Removed script from VC, add remote raw version
    
    * sample: include the latest version of the library
    
    * Update version on readme
    
    * React to Snackbar dismiss (#9)
    
    * Fix #8
    
    * Compile with project
    
    * Fix #11 (#12)
    
    * Library versions bump
    
    * Update readme version bump, ordering xml

--- faboptions/src/main/java/com/joaquimley/faboptions/FabOptionsBehavior.java
+++ faboptions/src/main/java/com/joaquimley/faboptions/FabOptionsBehavior.java
@@ -19,24 +19,30 @@ package com.joaquimley.faboptions;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.view.View;
+import android.widget.FrameLayout;
 
 /**
  * FabOptions component default CoordinatorLayout.Behavior to react Snackbar
  */
 
-public class FabOptionsBehavior extends CoordinatorLayout.Behavior {
+public class FabOptionsBehavior extends CoordinatorLayout.Behavior<FrameLayout> {
 
     @Override
-    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+    public boolean layoutDependsOn(CoordinatorLayout parent, FrameLayout child, View dependency) {
         return dependency instanceof Snackbar.SnackbarLayout;
     }
 
     @Override
-    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
+    public boolean onDependentViewChanged(CoordinatorLayout parent, FrameLayout child, View dependency) {
         float translationY = Math.min(0, dependency.getTranslationY() - dependency.getHeight());
         child.setTranslationY(translationY);
         // TODO: 21/11/2016 Handle instanceof Bottomsheetnav & others
         return true;
     }
-    //FabOptions component default {@link CoordinatorLayout.Behavior} to react to {@link Snackbar}
+
+    @Override
+    public void onDependentViewRemoved(CoordinatorLayout parent, FrameLayout child, View dependency) {
+        super.onDependentViewRemoved(parent, child, dependency);
+        child.setTranslationY(0);
+    }
 }
